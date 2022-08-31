f19356da72c38e71358297e2eea8f0ab2c0082d1
decodeseb <seb9Villiers@gmail.com>
2022-08-30 23:21:17 -0400

    add coding challenge completed on 08-30

---
+++ 8kyu/2022-08-30/combine-strings.js
@@ -0,0 +1,5 @@
+//Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+//Example: combineNames('James', 'Stevens')
+
+const combineNames = (first, last) => first + ' ' + last;
