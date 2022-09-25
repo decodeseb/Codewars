8cdec4a1d1fa513369b806fee90dbca45cc5672d
decodeseb <seb9Villiers@gmail.com>
2022-09-25 14:45:32 -0400

    add refactored solution using a Set

--- 8kyu/2022-09-25/return-2-highest-values-in-list.js
+++ 8kyu/2022-09-25/return-2-highest-values-in-list.js
@@ -8,7 +8,10 @@ Examples:
 [1, 1, 1]  =>  [1]
 []  =>  [] */
 
-let max1, max2;
+
+
+//Orinial solution
+/* let max1, max2;
   if(arr.length < 2){
     return arr
   } else {
@@ -16,4 +19,16 @@ let max1, max2;
     arr = arr.map(num => num === max1 ? delete num : num)
     max2= Math.max(...arr)
     return [max1, max2]
+  }
+ */
+
+
+
+
+  //Refactoring: Using a Set is much more eficient and D.R.Y. than my previous code. The set filters out non-unique
+  //values and we can then place them in another array in order to sort and find the first 2 values.
+
+  function twoHighest(arr) {
+    arr = [...new Set(arr)];
+    return arr.sort((a, b) => b - a).slice(0, 2)
   }
