032a5b36b1ebf9c749e9cfd615241d0d45351103
decodeseb <seb9Villiers@gmail.com>
2022-10-03 12:07:49 -0400

    add coding challenge completed on 10-03

---
+++ 8kyu/2022-10-03/a-bugs-trilogy.js
@@ -0,0 +1,11 @@
+function yourFutureCareer() { // a simple function declaration without parameters.
+    var career = Math.random(); // initializes a variable and assigns it a random number
+      if (career <= 0.32)
+        return 'FrontEnd Developer'
+      else if (career <= 0.65) 
+        return 'BackEnd Developer'
+      else return 'Full-Stack Developer'
+      }
+  
+  
+  yourFutureCareer(); // this is the function call
