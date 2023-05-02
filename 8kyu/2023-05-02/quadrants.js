fe476bbc79c6d670821856d23ac234a5b956c024
decodeseb <seb9Villiers@gmail.com>
2023-05-02 15:41:56 -0400

    add coding challenge completed on 2023-05-02

---
+++ 8kyu/2023-05-02/quadrants.js
@@ -0,0 +1,24 @@
+/* Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y 
+are non-zero integers, therefore the given point never lies on the axes.
+
+Examples
+(1, 2)     => 1
+(3, 5)     => 1
+(-10, 100) => 2
+(-1, -9)   => 3
+(19, -56)  => 4 */
+
+function quadrant(x, y) {
+    // parameters: two non-zero integers.
+    // return: the number of a quadrant depending on if each num is positive or negative
+    // examples: (-7, 10) -> 2 or (4, -5) -> 4
+    // pseudo-code : using conditionals we can create an outpout the is contingent upon the first or second number being a combination of true or false
+    
+    if(x > 0 && y > 0){
+      return 1;
+    } else if(x < 0 && y > 0){
+      return 2;
+    }else if (x < 0 && y < 0){
+      return 3;
+    } else return 4;
+  }
