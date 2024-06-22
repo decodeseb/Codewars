f49ebe0bfcba3f2dc85ffd5290bc1b705179cb32
decodeseb <seb9Villiers@gmail.com>
2024-06-21 23:11:43 -0400

    add coding challenge completed on 2024-06-21

---
+++ 7kyu/2024-06-21/remove-duplicate-word.js
@@ -0,0 +1,17 @@
+/* Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'
+*/
+
+function removeDuplicateWords (s) {
+    let newS = s.split(' ')
+    return Array.from(new Set(newS)).join(' ');
+  }
