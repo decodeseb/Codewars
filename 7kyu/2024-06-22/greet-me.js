00cba694b1c01cfc3e7443862f402ea32352cdc8
decodeseb <seb9Villiers@gmail.com>
2024-06-22 22:41:29 -0400

    add coding challenge completed on 2024-06-22

---
+++ 7kyu/2024-06-22/greet-me.js
@@ -0,0 +1,12 @@
+/* DESCRIPTION:
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!" */
+
+var greet = function(name) {
+    name = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()
+    return `Hello ${name}!`
+  };
