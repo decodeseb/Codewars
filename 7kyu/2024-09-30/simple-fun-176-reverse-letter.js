17e8602b70225dc9b3d31ad7699dba445859b758
decodeseb <seb9Villiers@gmail.com>
2024-09-30 20:03:25 -0400

    add coding challenge completed on 2024-09-30

---
+++ 7kyu/2024-09-30/simple-fun-176-reverse-letter.js
@@ -0,0 +1,13 @@
+/* 
+Task
+
+Given a string str, reverse it and omit all non-alphabetic characters.
+Example
+
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu". */
+
+function reverseLetter(str) {
+    return str.split('').reverse().join('').replace(/[^a-z]/gi, '')
+  }
