53884c533e24c559c78013e7ee33716276b63169
decodeseb <seb9Villiers@gmail.com>
2024-12-09 11:14:42 -0500

    add coding challenge completed on 2024-12-09

---
+++ 6kyu/2024-12-09/delete-occ-of-n.js
@@ -0,0 +1,23 @@
+/* Task
+
+Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+With list [20,37,20,21] and number 1, the result would be [20,37,21].
+Lists
+Fundamentals */
+
+function deleteNth(arr,n){
+    let tracker = {};
+    let final = [];
+    for(let i = 0; i < arr.length; i++) {
+      if(tracker[arr[i]]){
+        if(tracker[arr[i]] < n)
+          final.push(arr[i]);
+          tracker[arr[i]] += 1;
+      } else {
+        final.push(arr[i]);
+        tracker[arr[i]] = 1;
+      }
+    };
+    return final;
+  }
